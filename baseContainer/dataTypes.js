b895e749b5348d653163aac82f0c710dd47f5fbd
PriyaVerma2000 <146624815+PriyaVerma2000@users.noreply.github.com>
2023-10-01 14:16:31 +0000

    dataType Commit

---
+++ baseContainer/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+//alert("hello");
+
+//tc39.es/ecma262/#sec-white-space[global object, numbers and date]
+//mdn
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = true
+let state
+/*
+primitive datatype---
+Number => 2 to power 53
+bigint 
+string => ""
+boolean=> true false
+null => standalone value object
+undefined => variable declared but havent defined
+symbol = unique 
+
+*/
+console.log(typeof "hitesh");
+console.log(typeof null);
---
+++ baseContainer/variables.js
@@ -0,0 +1,10 @@
+const account_Id =100
+let account_Email ="priya.verma@google.com"
+var account_Password = "password123"
+account_City = "Jaipur"
+
+/*
+var create issue in block scope and functional scope 
+so todays time we use let over var
+ */
+console.table([account_Id,account_Email,account_Password,account_City])
