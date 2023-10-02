5cc364e30063ac673b1e6d08eccac3a4531b0cfb
PriyaVerma2000 <146624815+PriyaVerma2000@users.noreply.github.com>
2023-10-02 08:58:27 +0000

    math and number commit

---
+++ baseContainer/numb_math.js
@@ -0,0 +1,37 @@
+// const score = 100
+// console.log(score)
+// const balance = new Number(5000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 44.34343
+
+// console.log(otherNumber.toPrecision(3));
+
+// const num = 500000
+// console.log(num.toLocaleString('en-IN'));
+
+//MATH
+
+//console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.ceil(6.6));
+// console.log(Math.floor(5.2));
+// console.log(Math.round(5.5));
+// console.log(Math.min(5,6,4,3,2));
+// console.log(Math.max(5,15,4,3,2));
+
+console.log(Math.random())//always give value between 0-1 (0.1,0.3)
+console.log(Math.random()*10+1);//to get value above 0 so added +1 to get value between 1 to 9
+console.log((Math.random()*10)+1);//for confirm value
+
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)//to get the range between max-min then +1 to avoid 0 case
+
+
