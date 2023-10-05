a530c72002f97cf7c66c75246c470374b3713560
PriyaVerma2000 <146624815+PriyaVerma2000@users.noreply.github.com>
2023-10-05 16:23:23 +0000

    container2Arrays

---
+++ baseContainer2/arrays.js
@@ -0,0 +1,10 @@
+const myArr = [0,1,3,3,5,6]
+/*js arrays are resizable and can contain mix of data type
+//js array-copy-operation create shallow copies, shallow copy of an object is a copy whose property share the same reference,
+and whatever changes we make which changes in original arrays too
+//Deep copy of an object is a copy whose property doesn't share the reference 
+*/
+const studentName = ["Ritz", "Adam", "Rachel"]
+
+const myArr2 = new Array(1,2,4,5,6)
+console.log(myArr[0]);
